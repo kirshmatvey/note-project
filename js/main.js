1d9a4fcd84edf776538254527bf0f09176b209bf
kirshmatvey <matveykirshchin01@gmail.com>
2025-12-21 19:58:41 +0400

    made adding notes possible

--- js/main.js
+++ js/main.js
@@ -16,21 +16,21 @@ const MOCK_NOTES = [
     },
     // ...
     {
-        id: 1,
+        id: 2,
         title: 'Работа с формами',
         content: 'К определённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте name',
         color: colors.RED,
         isFavorite: false,
     },
     {
-        id: 1,
+        id: 3,
         title: 'Работа с формами',
         content: 'К определённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте name',
         color: colors.BLUE,
         isFavorite: false,
     },
     {
-        id: 1,
+        id: 4,
         title: 'Работа с формами',
         content: 'К определённым полям формы можно обратиться через form.elements по значению, указанному в атрибуте name',
         color: colors.BLUE,
@@ -41,7 +41,7 @@ const MOCK_NOTES = [
 const model = {
     notes: MOCK_NOTES,
 
-    addNote(title, content) {
+    addNote(title, content, color) {
         const noteTitle = document.querySelector('#note-title')
         const noteDescription = document.querySelector('#note-description')
 
@@ -49,7 +49,7 @@ const model = {
             id: new Date().getTime(),
             title: title,
             content: content,
-            color: colors.BLUE,
+            color: color,
             isFavorite: false,
         }
 
@@ -66,13 +66,29 @@ const view = {
     render(data) {
         const noteContainer = document.querySelector('.note-container')
         const noteCounter = document.querySelector('.note-counter')
-
         noteCounter.textContent = `Всего заметок: ${data.length}`
 
         noteContainer.innerHTML = ''
 
-        data.forEach((item) => {
-            noteContainer.innerHTML += `
+        if (data.length === 0) {
+            const noNoteMessage = document.createElement('p')
+            noNoteMessage.innerHTML = `У вас нет еще ни одной заметки<br>Заполните поля выше и создайте свою первую заметку!`
+            noNoteMessage.classList.add('no-note-message')
+            noteContainer.append(noNoteMessage)
+        } else {
+            const noteContainerWrapper = document.createElement('div')
+            noteContainerWrapper.classList.add('note-container-wrapper')
+
+            const showFavorite = document.createElement('div')
+            showFavorite.classList.add('show-favorite')
+            showFavorite.innerHTML = `
+            <img src="./images/icons/checkbox-inactive.svg" class="favorite-checkbox" alt="show-favorite">
+            <span class="favorite-span">Показать только избранные заметки</span>
+            `
+            noteContainerWrapper.innerHTML = ''
+
+            data.forEach((item) => {
+                noteContainerWrapper.innerHTML += `
                 <article class="note">
                     <header class="note-header ${item.color}" >
                         <div class="note-header-wrapper">
@@ -87,34 +103,69 @@ const view = {
                         <p class="note-info">${item.content}</p>
                     </div>
                 </article>
-            `
-        })
+                `
+            })
+
+            noteContainer.append(showFavorite)
+            noteContainer.append(noteContainerWrapper)
+        }
+
     },
 
     init() {
+        let chosenColor = colors.YELLOW
         this.render(model.notes)
 
-        const additionButton = document.querySelector('.note-form')
+        const noteForm = document.querySelector('.note-form')
         const noteTitle = document.querySelector('#note-title')
         const noteDescription = document.querySelector('#note-description')
+        const noteHeader = document.querySelector('.note-header')
 
-        additionButton.addEventListener('submit', (event) => {
-            event.preventDefault()
-            controller.addNote(noteTitle.value, noteDescription.value)
+        noteHeader.addEventListener('click', (event) => {
+            if (event.target.getAttribute('id') === 'trash') {
+                controller.deleteNote(event.target.getAttribute('id'))
+            }
         })
 
+        noteForm.addEventListener('submit', (event) => {
+            event.preventDefault()
+            controller.addNote(noteTitle.value, noteDescription.value, chosenColor)
+        })
+
+        noteForm.addEventListener('click', (event) => {
+            if (event.target.classList.contains('radio')) {
+                switch (event.target.getAttribute('value')) {
+                    case 'green':
+                        chosenColor = colors.GREEN
+                        break;
+                    case 'blue':
+                        chosenColor = colors.BLUE
+                        break;
+                    case 'red':
+                        chosenColor = colors.RED
+                        break;
+                    case 'purple':
+                        chosenColor = colors.PURPLE
+                        break;
+                    case 'yellow':
+                        chosenColor = colors.YELLOW
+                        break;
+                }
+
+            }
+        })
     }
 
 }
 
 const controller = {
-    addNote(title, content) {
+    addNote(title, content, color) {
         if (title === '' || content === '') {
             // messageBox.classList.toggle('pop-up')
         } else if (title.length > 50) {
             // messageBox.classList.toggle('pop-up')
         } else {
-            model.addNote(title, content)
+            model.addNote(title, content, color)
         }
     }
 }
